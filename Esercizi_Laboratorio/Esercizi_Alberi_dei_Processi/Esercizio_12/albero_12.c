22d0e76330632e92757b6abf1915175343f415d5
alexdist <150511227+alexdist@users.noreply.github.com>
2025-10-08 07:41:39 +0200

    Add files via upload

---
+++ Esercizi_Laboratorio/Esercizi_Alberi_dei_Processi/Esercizio_12/albero_12.c
@@ -0,0 +1,22 @@
+// ALBERO 12
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int glob = 5;
+
+int main() {
+    pid_t pid;
+    int i;
+
+    for (i = 1; i < glob; i++) {
+        pid = fork();
+
+        if (pid == 0)
+            glob = glob - 1;
+    }
+
+    printf("Valore di glob = %d\n", glob);
+
+    return 0;
+}
